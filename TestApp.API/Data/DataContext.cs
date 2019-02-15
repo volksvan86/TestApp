77acfca95f181db8e91dcb70cd8ba2ac240f757e
Matthew Steiner <msteiner@parker.com>
2019-02-15 17:05:13 -0500

    Initial Commit

---
+++ TestApp.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using TestApp.API.Models;
+
+namespace TestApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Value> Values { get; set; }
+    }
+}
